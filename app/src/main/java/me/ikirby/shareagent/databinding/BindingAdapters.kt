551b22af15038fc5f000eb7994c23f1af8e5ef71
iKirby <ikirby5556660@gmail.com>
2021-02-06 21:39:23 +0800

    Remove useless log

--- app/src/main/java/me/ikirby/shareagent/databinding/BindingAdapters.kt
+++ app/src/main/java/me/ikirby/shareagent/databinding/BindingAdapters.kt
@@ -1,12 +1,10 @@
 package me.ikirby.shareagent.databinding
 
-import android.util.Log
 import android.view.View
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("app:show")
 fun bindShow(view: View, show: Boolean) {
-    Log.d("bindShow", "set visible = $show")
     if (show) {
         view.visibility = View.VISIBLE
     } else {
