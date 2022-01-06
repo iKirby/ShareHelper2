2c14b663add719a5aea163cbee307eab3c301cdd
iKirby <ikirby5556660@gmail.com>
2022-01-06 21:33:54 +0800

    Update user agent

--- app/src/main/java/me/ikirby/shareagent/util/NetworkUtil.kt
+++ app/src/main/java/me/ikirby/shareagent/util/NetworkUtil.kt
@@ -5,7 +5,7 @@ import org.jsoup.nodes.Document
 import java.io.IOException
 
 const val USER_AGENT =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
 
 @Throws(IOException::class)
 private fun getDocument(url: String): Document {
